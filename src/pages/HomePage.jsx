21ac7c406d645605b4d7a04d4e4db31774e6c258
Zolboo <zolboo@Zolboos-MacBook-Air.local>
2022-11-15 18:41:18 +0800

    pagination

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,87 +9,68 @@ const baseUrl = "https://dummyapi.io/data/v1/";
 export const HomePage = () => {
   const [pos, setPos] = useState(0);
   const [data, setData] = useState(null);
+  const [page, setPage] = useState(0);
+  const [limit, setLimit] = useState(10);
 
   const goRight = () => setPos((prev) => prev + 1);
   const goLeft = () => setPos((prev) => prev - 1);
 
+  const nextPage = () => {
+    setData(null);
+    setPage((prev) => prev + 1);
+  };
+  const prevPage = () => {
+    setData(null);
+    setPage((prev) => {
+      if (prev > 0) return prev - 1;
+    });
+  };
+
   useEffect(() => {
     axios
-      .get(baseUrl + "user", {
+      .get(baseUrl + `user?limit=${limit}&page=${page}`, {
         headers: {
           "app-id": "636f2f24e8d0ffd9c83fc52f",
         },
       })
       .then((res) => {
-        setData(res.data);
+        setData(res.data.data);
+        console.log(res.data);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [page, limit]);
 
   return (
-    <div>
-      {/* Section 1 */}
-
-      <div className={styles.sectionOne}>
-        <Container>
-          <div className={styles.card}>
-            <h1>Non nostrud non non tempor proident ut pariatur dolor.</h1>
-            <p>
-              Adipisicing eiusmod laborum nulla quis do. Id ad dolor laboris
-              aliqua cupidatat commodo consequat irure sit enim. Cillum deserunt
-              nostrud elit anim dolore duis id anim tempor. Minim Lorem
-              reprehenderit proident cillum deserunt occaecat dolor nostrud
-              sunt.
-            </p>
-            <div>
-              <input />
-              <Button></Button>
-            </div>
-          </div>
-        </Container>
-      </div>
-
-      {/* Section 2 */}
-
-      <div className={styles.sectionTwo}>
-        <Container>
-          <div className={styles.cards}>
-            <div
-              style={{
-                transform: `translateX(${-10 * pos}%)`,
-                transition: "300ms",
-              }}
-            >
-              {new Array(10).fill(0).map((_, index) => (
-                <Item index={index} />
-              ))}
-            </div>
-          </div>
-          <Button onClick={goLeft}>Left</Button>
-          <Button onClick={goRight}>Right</Button>
-        </Container>
-      </div>
-
-      {/* Section 3 */}
-
-      <div>
+    <div style={{ padding: "80px 0px" }}>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          gap: 24,
+        }}
+      >
         {!data && <Spinner />}
-        {data && data.data.map((item) => <Item {...item} />)}
+        {data && data.map((item) => <Item key={item.id} {...item} />)}
       </div>
+
+      <Button onClick={prevPage}>prev</Button>
+      <Button onClick={nextPage}>next</Button>
     </div>
   );
 };
 
 const Item = (props) => {
   return (
-    <Card style={{ width: 300, marginRight: 16 }}>
-      <Card.Body>
-        <Card.Title>{props.firstName}</Card.Title>
-        <Card.Text>{props.text}</Card.Text>
-        <Button variant="primary">{props.buttonText}</Button>
-      </Card.Body>
-    </Card>
+    <div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+      <Card style={{ width: 300, marginRight: 16 }}>
+        <Card.Body>
+          <Card.Title>{props.firstName}</Card.Title>
+          <Card.Text>{props.text}</Card.Text>
+          <Button variant="primary">{props.buttonText}</Button>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
